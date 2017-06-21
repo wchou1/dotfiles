c567a295806ae16942d23feb443ea1b1e01d8f74
Wistan Chou <wistanchou97@gmail.com>
2017-06-21 14:33:44 -0400

    changed location

--- fetch.sh
+++ fetch.sh
@@ -4,5 +4,5 @@ alias cp="cp -i"
 cp ~/.config/nvim/init.vim ./editor/init.vim
 cp ~/.tmux.conf ./mux/tmux.conf
 cp ~/.tmuxline.conf ./mux/tmuxline.conf
-cp ~/.zshrc ./editor/zshrc
+cp ~/.zshrc ./shell/zshrc
 cp ~/.gitconfig ./vcs/gitconfig
